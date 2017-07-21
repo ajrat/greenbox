db98b184a7182d6d526161ad6e548daf71a07dc2
ajrat <ayrat.mub@gmail.com>
2017-07-22 01:41:11 +0300

    Первый index

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+function sendMessage($text) {
+file_get_contents("https://api.telegram.org/bot372340784:AAEGVQ7MwgcVbWD5Q9zH6eQn1VT5KNx-QFQ/sendMessage?chat_id=288119914&text=". urlencode($text));
+};
+
+
+
+
+
+if (!empty($_GET["sendstate"]) {
+	$freshstate = file_get_contents('freshstate');
+	echo $freshstate;
+}
+
+/*
+
+$skey = $_GET["skey"];
+$t_air = $_GET["t_air"];
+$h_air = $_GET["h_air"];
+
+if ($skey == "LoKittyXTC") {
+	file_put_contents ('freshdata.txt', "t = $t_air;h = $h_air;");
+};
+
+
+/*
+$skey = $_GET["skey"];
+$t_air = $_GET["t_air"];
+$h_air = $_GET["h_air"];
+$h_ground = $_GET["h_ground"];
+*/
+
+
+/*$t_air = 22;
+$h_air = 11;*
+//if ($skey == "LoKittyXTC") {
+	//file_put_contents ('file.txt', "t = $t_air;h = $h_air;g = $h_ground;\n", FILE_APPEND);
+ $message="t = $t_air;\nh = $h_air;";
+
+sendMessage($message)
+
+ /*file_get_contents("https://api.telegram.org/bot372340784:AAEGVQ7MwgcVbWD5Q9zH6eQn1VT5KNx-QFQ/sendMessage?chat_id=288119914&text=urlencode($message)");
+*/
+//}
+
+
+
+
+
+
+?>
