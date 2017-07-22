6c2de5d108036012dc67eac676ce75bc3c110342
ajrat <ayrat.mub@gmail.com>
2017-07-22 06:28:19 +0300

    ver 0.1

---
+++ arduino.php
@@ -0,0 +1,19 @@
+<?php
+	$skey = "LoKittyXTC";
+	if (!empty($_GET["skey"])) {
+		if ($_GET["skey"]==$skey) {
+			$t_air = $_GET["t_air"];
+			$h_air = $_GET["h_air"];
+			$h_ground = $_GET["h_ground"];
+			$lamps = $_GET["lamps"];
+			if ($lamps=="0.00") {
+				$lamps="Выключены";
+			}
+			if ($lamps=="1.00") {
+				$lamps="Включены";
+			}			
+			file_put_contents ('freshstate.txt', $t_air.";".$h_air.";".$h_ground.";".$lamps.";");
+			echo "true";
+		};
+	};
+?>
