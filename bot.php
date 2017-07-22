d1e33c0a5ba9189d03bfa8100ea323bbcc8a7ace
ajrat <ayrat.mub@gmail.com>
2017-07-22 04:10:02 +0300

    bot.php для телеграм простая команда тест

--- bot.php
+++ bot.php
@@ -1,3 +1,46 @@
 <?php
+$access_token = '372340784:AAEGVQ7MwgcVbWD5Q9zH6eQn1VT5KNx-QFQ';
+$api = 'https://api.telegram.org/bot' . $access_token;
+
+// Получим то, что передано скрипту ботом в POST-сообщении и распарсим
+$output = json_decode(file_get_contents('php://input'), TRUE); 
+
+$chat_id = $output['message']['chat']['id']; // Выделим идентификатор чата
+$first_name = $output['message']['chat']['first_name']; // Выделим имя собеседника
+$message = $output['message']['text'];
+
+switch(strtolower_ru($message)) {
+	case ('привет'):
+	case ('/hello'):
+	sendMessage($chat_id, 'Привет, '. $first_name . '! ' . $emoji['preload'] );
+  break;
+case ('/start'):
+break;
+default:
+sendMessage($chat_id, 'Неизвестная команда!' );
+break;
+}
+
+
+function sendMessage($chat_id, $message) {
+
+file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
+
+}
+
+/**
+* Функция перевода символов в нижний регистр, учитывающая кириллицу
+*/
+
+function strtolower_ru($text) {
+
+   $alfavitlover = array('ё','й','ц','у','к','е','н','г', 'ш','щ','з','х','ъ','ф','ы','в', 'а','п','р','о','л','д','ж','э', 'я','ч','с','м','и','т','ь','б','ю');
+
+     $alfavitupper = array('Ё','Й','Ц','У','К','Е','Н','Г', 'Ш','Щ','З','Х','Ъ','Ф','Ы','В', 'А','П','Р','О','Л','Д','Ж','Э', 'Я','Ч','С','М','И','Т','Ь','Б','Ю');
+
+return str_replace($alfavitupper,$alfavitlover,strtolower($text));
+
+}
+
 
 ?>
