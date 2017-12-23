9073c7c329bff271194983a707351e776343af75
ajrat <ayrat.mub@gmail.com>
2017-12-23 15:37:38 +0300

    rwrwrw

--- telegram.php
+++ telegram.php
@@ -4,6 +4,8 @@
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 $chatID = $update["message"]["chat"]["id"];
+$timertrigger = false;
+
 
 if (!$chatID) die("no");
 
@@ -12,6 +14,7 @@ $replyMarkup['keyboard'][] = array("Включить лампы","Выключи
 $replyMarkup['keyboard'][] = array("Расписание"); 
 $replyMarkup['keyboard'][] = array("Датчики"); 
 
+if ($timertrigger == false) {
 if (substr_count($update["message"]["text"],'Включить лампы')) {
 	$timerdata = file_get_contents('timer.txt');
 	$timerarray = explode(";", $timerdata);
@@ -32,6 +35,14 @@ if (substr_count($update["message"]["text"],'Выключить лампы')) {
 
 	file_put_contents ('lamps.txt', "off");
 	$msg = "Выключил";
+}
+
+if (substr_count($update["message"]["text"],'Расписание')) {
+	$timertrigger = true;
+	$msg = "Введи время (XX:XX;YY:YY)";
+}
+}else{
+	file_put_contents ('timer.txt', $update["message"]["text"].'1');	
 }
 
 $sendto ="https://api.telegram.org/bot$botkey/sendmessage?parse_mode=HTML&chat_id=".$chatID."&text=".urlencode($msg)."&disable_web_page_preview=true&reply_markup=".json_encode($replyMarkup);
