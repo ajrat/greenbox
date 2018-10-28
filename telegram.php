7cbeb8ea2c209b5337cb62c7586798388e7fd77c
Квадрат <Ayrat.mub@gmail.com>
2018-10-28 16:47:08 +0300

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -16,15 +16,17 @@ $replyMarkup['keyboard'][] = array("Расписание");
 $replyMarkup['keyboard'][] = array("Датчики"); 
 
 
-/*
-$temptimevar = preg_split('//u', $update["message"]["text"], NULL, PREG_SPLIT_NO_EMPTY);
-if (($temptimevar[2]=":")&&($temptimevar[5]="-")&&($temptimevar[8]=":")) {
+if ($timertrigger == false) {
+	# code...
+	$temptimevar = preg_split('//u', $update["message"]["text"], NULL, PREG_SPLIT_NO_EMPTY);
+	if (($temptimevar[2]=":")&&($temptimevar[5]="-")&&($temptimevar[8]=":")) {
 
-	file_put_contents ('timer.txt', $temptimevar[0].$temptimevar[1].":".$temptimevar[3].$temptimevar[4].";".$temptimevar[6].$temptimevar[7].":".$temptimevar[9].$temptimevar[10].";"."1");
-	$msg = "Таймер установлен";
-	//9:00;04:00;1
+		file_put_contents ('timer.txt', $temptimevar[0].$temptimevar[1].":".$temptimevar[3].$temptimevar[4].";".$temptimevar[6].$temptimevar[7].":".$temptimevar[9].$temptimevar[10].";"."1");
+		$timertrigger = false;
+		$msg = "Таймер установлен";
+		//9:00;04:00;1
+	}
 }
-*/
 
 if (substr_count($update["message"]["text"],'Включить лампы')) {
 	$timerdata = file_get_contents('timer.txt');
@@ -51,11 +53,6 @@ if (substr_count($update["message"]["text"],'Выключить лампы')) {
 if (substr_count($update["message"]["text"],'Расписание')) {
 	$timertrigger = true;
 	$msg = "Введи время (<XX:XX-YY:YY>)";
-	//file_put_contents ('rw.txt', '1');
-}
-if (substr_count($update["message"]["text"],'Расписание')) {
-	$timertrigger = true;
-	$msg = "Введи время (XX:XX;YY:YY)";
 	//file_put_contents ('rw.txt', '1');
 }
 
