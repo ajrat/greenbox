dcdd459ae6e8e5d6120632282666c77d43810d42
Квадрат <Ayrat.mub@gmail.com>
2018-10-28 17:17:23 +0300

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -17,9 +17,10 @@ $replyMarkup['keyboard'][] = array("Датчики");
 
 
 $temptimevar = preg_split('//u', $update["message"]["text"], NULL, PREG_SPLIT_NO_EMPTY);
-if (($temptimevar[2]=":")&&($temptimevar[5]=";")&&($temptimevar[8]=":")) {
 
-	file_put_contents ('timer.txt', $temptimevar[0].$temptimevar[1].":".$temptimevar[3].$temptimevar[4].";".$temptimevar[6].$temptimevar[7].":".$temptimevar[9].$temptimevar[10].";");
+
+if ( ($temptimevar[2]=":")&&($temptimevar[5]=";")&&($temptimevar[8]=":") ) {
+	//file_put_contents ('timer.txt', $temptimevar[0].$temptimevar[1].":".$temptimevar[3].$temptimevar[4].";".$temptimevar[6].$temptimevar[7].":".$temptimevar[9].$temptimevar[10].";");
 	$msg = "Таймер установлен";
 }else
 
